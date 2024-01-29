6abcbe250718b27571d33eafcb382f7d3efe0510
GreyRabbitty <GreyRabbitty@gmail.com>
2024-01-29 16:46:10 +0100

    Resolve the spelling issues

--- components/race/RaceComment.tsx
+++ components/race/RaceComment.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 export default function RaceComment() {
     return (
         <div className='flex flex-col text-center items-center gap-2 font-Josefin pt-[156px] pb-[180px] text-white'>
-            <h1 className='text-5xl'>Genesis Races</h1>
-            <p className='w-full md:w-[480px] px-5'>
+            <h1 className='text-5xl'>The Races in Galactic Kingdom</h1>
+            {/* <p className='w-full md:w-[480px] px-5'>
                 How the genesis reborn to be so powerful?
-            </p>
+            </p> */}
         </div>
     )
 }
