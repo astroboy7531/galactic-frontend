405527bee1d89a2dbcd784db0185e3bb280e9859
Lazar Jankovic <GreyRabbitty@gmail.com>
2024-02-25 21:27:57 +0100

    update

--- components/FaqComp.tsx
+++ components/FaqComp.tsx
@@ -8,7 +8,7 @@ type AccordionProps = {
 const FaqComp = ({ title, content }: AccordionProps) => {
     const [isOpened, setOpened] = useState<boolean>(false)
     const [height, setHeight] = useState<string>("0px")
-    const contentElement = useRef(1)
+    const contentElement = useRef(null)
 
     const HandleOpening = () => {
         setOpened(!isOpened)
@@ -18,11 +18,11 @@ const FaqComp = ({ title, content }: AccordionProps) => {
         <div onClick={HandleOpening} className=" mb-4 w-full cursor-pointer border-[3px] dark:bg-gray-200 gradient-border rounded-xl">
             <div className={" h-[4.5rem] max-sm:h-14 p-8 flex justify-between items-center text-black rounded-xl"}>
                 <h4 className="font-bold">{title}</h4>
-                {isOpened ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor" className="w-6 h-6 rotate-45 duration-300 transition-all">
+                {isOpened ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor" className="w-6 h-6 transition-all duration-300 rotate-45">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                 </svg>
 
-                    : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor" className="w-6 h-6 duration-300 transition-all">
+                    : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor" className="w-6 h-6 transition-all duration-300">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                     </svg>
 
@@ -31,7 +31,7 @@ const FaqComp = ({ title, content }: AccordionProps) => {
             <div
                 ref={contentElement}
                 style={{ height: height }}
-                className="bg-white/10 text-black overflow-hidden transition-all duration-300 rounded-b-xl"
+                className="overflow-hidden text-black transition-all duration-300 bg-white/10 rounded-b-xl"
             >
                 <p className="p-4">{content}</p>
             </div>
